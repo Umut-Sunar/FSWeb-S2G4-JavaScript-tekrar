6c26d4bb7cba20202c35c0169521c5f98c64b600
Umut-Sunar <doganumutsunar@gmail.com>
2024-01-15 10:42:54 +0300

    Çözümler Ara
    3f 'ye kadar olan çözümler commit edilmiştir.

--- index.js
+++ index.js
@@ -36,9 +36,10 @@ const sayilar = [
 */
 
 //Örneğin çözümü:
+
 function KareninAlani(kenaruzunlugu) {
   return kenaruzunlugu * kenaruzunlugu;
-}
+} 
 
 /* (Oto test yok) Yukarıdaki KareninAlani fonksiyonunu kenar uzunluğu = 10 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 
@@ -50,9 +51,9 @@ function KareninAlani(kenaruzunlugu) {
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
-function CemberinCevresi(/* kodlar buraya */) {
-  /* kodlar buraya */
-}
+const  CemberinCevresi = (yaricap) =>  {return 2*pi*yaricap }
+
+
 
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 
@@ -64,15 +65,18 @@ function CemberinCevresi(/* kodlar buraya */) {
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinAlani(yarıçap) {
+        return pi*(Math.pow(yarıçap,2))
 }
+
 
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 
 /* 	GÖREV 3:
-	- Sayfanın en üstünde global değişken olarak tanımlanmış bir sayilar dizisi bulunmaktadır. Bu dizi içinde 0 ile 1000 arasında rasgele oluşturulmuş tam sayılar ve ondalıklı sayılar bulunmaktadır. Bu diziyi kullanarak aşağıdakileri uygulayın:
-		3a. enbuyuk ve enkucuk isminde 2 adet değişken tanımlayın ve sayilar dizisindeki en küçük sayı ile en büyük sayıyı bu değişkenlere atayın. (for döngüsü kullanın)
+	- Sayfanın en üstünde global değişken olarak tanımlanmış bir sayilar dizisi bulunmaktadır. 
+  Bu dizi içinde 0 ile 1000 arasında rasgele oluşturulmuş tam sayılar ve ondalıklı sayılar bulunmaktadır. Bu diziyi kullanarak aşağıdakileri uygulayın:
+		3a. enbuyuk ve enkucuk i  sminde 2 adet değişken tanımlayın ve sayilar dizisindeki en küçük sayı ile endeğişkenlere atayın. (for döngüsü kullanın)
+      büyük sayıyı bu 
 		
 		3b. `ucetambolunenler` adında bir dizi tanımlayın ve bu diziye sayilar dizisindeki 3'ün tam katı olan sayıları atayın (.forEach metodunu kullanın)
 		
@@ -82,9 +86,12 @@ function CemberinAlani(/* kodlar buraya */) {
 		
 		3e. besyuzdenkucuksayilar dizisindeki sayıları küçükten büyüğe sıralayıp `siralisayilar` adındaki bir diziye aktarın (.sort metodunu kullanın)
 		
-		3f. `tekraredensayilar` adında bir dizi oluşturun. sayilar dizisi içerisindeki bazı sayılar birden fazla kere yazılmış. sayilar dizisi içerisinde birden fazla kez yazılmış sayıları tespit ederek kaç kere tekrar edildiğini belirten bir string oluşturulup `tekraredensayilar` dizisine aktarılmasını istiyoruz. Örnek string: "{sayı} sayısı {tekrarSayisi} kere tekrar edilmiştir"
+		3f. `tekraredensayilar` adında bir dizi oluşturun. sayilar dizisi içerisindeki bazı sayılar birden fazla kere yazılmış. 
+    sayilar dizisi içerisinde birden fazla kez yazılmış sayıları tespit ederek kaç kere tekrar edildiğini belirten bir string oluşturulup `tekraredensayilar` dizisine aktarılmasını istiyoruz.
+    Örnek string: "{sayı} sayısı {tekrarSayisi} kere tekrar edilmiştir"
 		ÖRNEK: sayilar dizisi içerisinde 45 sayısı 3 kere yazılmış. "45 sayısı 3 tekrar edilmiştir" stringini `tekraredensayilar` dizisine aktaracağız.
-		💡 İPUCU: Tekrar edilen sayıları ve kaç kere tekrar edildiğini kaydetmek için bir nesne tanımlamalısınız, bu görevi yapabilmek için en az 2 kere döngü yazmalısınız. Birinci döngüde hangi sayının kaç kere tekrar edildiğini tespit edip, 2. döngüde stringi oluşturup verilen diziye aktarmalısınız.
+		💡 İPUCU: Tekrar edilen sayıları ve kaç kere tekrar edildiğini kaydetmek için bir nesne tanımlamalısınız, 
+    bu görevi yapabilmek için en az 2 kere döngü yazmalısınız. Birinci döngüde hangi sayının kaç kere tekrar edildiğini tespit edip, 2. döngüde stringi oluşturup verilen diziye aktarmalısınız.
 */
 
 /*  (oto test yok) sayilar dizisi içinde kaç adet sayı olduğunu konsola yazdırın */
@@ -99,21 +106,69 @@ let ucetambolunenler,
 
 // 3a çözümü
 
-/* kodlar buraya */
+  enbuyuk = sayilar[0];
+  enkucuk = sayilar[0]; 
+
+  for (let i= 0 ; i<sayilar.length;i++) {
+
+    if(enkucuk>sayilar[i] & i<sayilar.length) {
+      enkucuk = sayilar[i]
+      
+    } 
+
+    if ( ( enbuyuk<sayilar[i+1]) && (i<sayilar.length)) {
+        enbuyuk = sayilar[i+1]
+        
+
+    }
+
+  }
+  
 
 // 3b çözümü:
+
+  ucetambolunenler = [] ;
+  sayilar.forEach( (num) => {
+
+    if (num%3 === 0 ) {
+
+      ucetambolunenler.push(num)
+
+    }
+      }
+       
+      
+      )
+      
+  
 
 /* kodlar buraya */
 
 // 3c çözümü:
 
+ucebolunenlerintoplami=ucetambolunenler.reduce(  (num,currentValue) =>  {
+
+    num += currentValue;
+    return num
+
+
+
+} , 0)
+
+
+
 /* kodlar buraya */
 
 // 3d çözümü
+besyuzdenkucuksayilar = sayilar.filter(  (num) => {  return num >500   })
+
 
 /* kodlar buraya */
 
 // 3e çözümü
+
+siralisayilar= besyuzdenkucuksayilar.sort( (a,b) => a-b   )
+
 
 /* kodlar buraya */
 
